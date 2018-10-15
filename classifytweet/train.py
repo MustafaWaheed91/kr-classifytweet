48f30f85bbe4ec5499edb13973283cd29605bb49
Waheed <muwaheed@8c85900957b9.ant.amazon.com>
2018-10-15 04:52:05 -0500

    update entry_point() function

--- classifytweet/train.py
+++ classifytweet/train.py
@@ -1,31 +1,28 @@
 import re
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd
 from keras.preprocessing import sequence
 from keras.preprocessing.text import Tokenizer
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, LSTM, Conv1D, MaxPooling1D, Dropout, Activation
 from keras.layers.embeddings import Embedding
-from matplotlib import pyplot as plt
 
 from classifytweet.resolve import paths
 
 
 def preprocess_tweet(tweet):
     """
-    preprocess the text in a single tweet
+    preprocess the text in a single tweet. convert all urls to sting "URL"
+    after that convert all @username to "AT_USER" then correct all multiple white spaces to a single white space
+    finally convert "#topic" to just "topic".
     :param tweet:
     :return: the clean version of that single tweet
     """
     tweet.lower()
-    #convert all urls to sting "URL"
     tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet)
-    #convert all @username to "AT_USER"
     tweet = re.sub('@[^\s]+','AT_USER', tweet)
-    #correct all multiple white spaces to a single white space
     tweet = re.sub('[\s]+', ' ', tweet)
-    #convert "#topic" to just "topic"
     tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
     tweet = re.sub(r'\W*\b\w{1,3}\b', '', tweet)
     return tweet
@@ -33,12 +30,11 @@ def preprocess_tweet(tweet):
 
 def entry_point():
     """
-
-    :return: initiates the keras training job and saved model.h5 file at the end
+    read data , describe model graph and finally train model
+    return: initiates the keras training job and saved model.h5 file at the end
     """
 
     dataframe = pd.read_csv(paths.input(channel='training', filename="training.1600000.processed.noemoticon.csv"), encoding="ISO-8859-1", header=None).iloc[:, [0, 4, 5]].sample(frac=1).reset_index(drop=True)
-    users = np.array(dataframe.iloc[:, 1].values)
     tweets = np.array(dataframe.iloc[:, 2].apply(preprocess_tweet).values)
     sentiment = np.array(dataframe.iloc[:, 0].values)
     print(tweets)
@@ -75,26 +71,13 @@ def entry_point():
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     model.summary()
 
-    model.fit(X, y, batch_size=128, verbose=1, validation_split=0.2, epochs=1   )
-
+    history = model.fit(X, y, batch_size=128, verbose=1, validation_split=0.2, epochs=2)
     model.save(paths.model(filename='model.h5'))
 
-    with open('file_to_write', 'w') as f:
-        s = "Accuracy " + history.history['acc'] + " train " + history.history['val_acc'] + " validation"
-        f.write(s)
-
-    plt.plot(history.history['loss'])
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.show()
-    plt.savefig('Figure 1')
-
-    plt.plot(history.history['acc'])
-    plt.xlabel('Accuracy')
-    plt.ylabel('Loss')
-    plt.show()
-    plt.savefig('Figure 1')
-
+    print("loss")
+    print(history.history['loss'])
+    print("accuracy")
+    print(history.history['acc'])
 
 
 if __name__ == '__main__':
